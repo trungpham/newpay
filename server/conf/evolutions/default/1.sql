66a1d89e2ce31c8c97ac0d62711173fdc53c139d
Trung Pham <trung@phamcom.com>
2013-03-06 22:39:13 -0800

    update sql

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -4,6 +4,8 @@
 
 CREATE TABLE User (
     id BIGINT (20) NOT NULL AUTO_INCREMENT,
+    firstName varchar (255),
+    lastName varchar (255),
     PRIMARY KEY (id)
 );
 
--- server/conf/evolutions/default/5.sql
+++ server/conf/evolutions/default/5.sql
@@ -6,7 +6,6 @@ CREATE TABLE PaymentProfile (
     id BIGINT (20) NOT NULL AUTO_INCREMENT,
     type VARCHAR (255),
     last4 VARCHAR (255),
-    pan VARCHAR (255),
     cardBrand VARCHAR (255),
     expiryMonth VARCHAR (255),
     expiryYear VARCHAR (255),
